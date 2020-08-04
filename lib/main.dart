fedd1c875ebdeae2c5892eb8b32840640d738e59
Elma Santos <elma.santos@nubank.com.br>
2020-08-04 13:29:23 -0300

    Add 'Create Transfer' screen and improve Transfer

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,123 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-  MaterialApp(
-      home: Scaffold(
-        body: Card(
-          child: Column(
-            children: <Widget>[
-              Text('test'),
-            ],
+void main() => runApp(BytebankApp());
+
+class BytebankApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+          body: TransferForm(),
+        )
+    );
+  }
+}
+
+class TransferForm extends StatelessWidget {
+
+  final TextEditingController _accountNumberFieldController = TextEditingController();
+  final TextEditingController _valueFieldController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Creating Transfer'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              controller: _accountNumberFieldController,
+              style: TextStyle(fontSize: 24.0),
+              decoration: InputDecoration(
+                labelText: 'Account number',
+                hintText: '0000',
+              ),
+              keyboardType: TextInputType.number,
+            ),
           ),
-        ),
-        appBar: AppBar(
-          title: Text('Transfers'),
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-        ),
-      )
-  ),
-);
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              controller: _valueFieldController,
+              style: TextStyle(fontSize: 24.0),
+              decoration: InputDecoration(
+                icon: Icon(Icons.monetization_on),
+                labelText: 'Value',
+                hintText: '0.00',
+              ),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          RaisedButton(
+            child: Text('Confirm'),
+            onPressed: () {
+              debugPrint(_accountNumberFieldController.text);
+
+              final int accountNumber = int.tryParse(_accountNumberFieldController.text);
+              final double value = double.tryParse(_valueFieldController.text);
+
+              if(accountNumber != null && value != null) {
+               final transferCreated = Transfer(value, accountNumber);
+               debugPrint('$transferCreated');
+              }
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class TransfersList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          TransferItem(Transfer(100.0, 1000)),
+          TransferItem(Transfer(200.0, 2000)),
+          TransferItem(Transfer(300.0, 7600)),
+        ],
+      ),
+      appBar: AppBar(
+        title: Text('Transfers'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class TransferItem extends StatelessWidget {
+
+  final Transfer _transfer;
+
+  TransferItem(this._transfer);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transfer.value.toString()),
+        subtitle: Text(_transfer.accountNumber.toString()),
+      ),
+    );
+  }
+}
+
+class Transfer {
+  final double value;
+  final int accountNumber;
+
+  Transfer(this.value, this.accountNumber);
+
+  @override
+  String toString() {
+    return 'Transfer{value: $value, accountNumber: $accountNumber}';
+  }
+}
