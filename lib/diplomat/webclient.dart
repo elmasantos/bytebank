320b1411970824df71cfd17788d62c75c88c80e6
Elma Santos <elma.santos@nubank.com.br>
2020-08-13 10:52:52 -0300

    Add HTTP interceptor

--- lib/diplomat/webclient.dart
+++ lib/diplomat/webclient.dart
@@ -1,6 +1,31 @@
 import 'package:http/http.dart';
+import 'package:http_interceptor/http_client_with_interceptor.dart';
+
+import 'package:http_interceptor/interceptor_contract.dart';
+import 'package:http_interceptor/models/request_data.dart';
+import 'package:http_interceptor/models/response_data.dart';
 
 void findAll() async {
-  final Response response = await get('http://10.0.2.2:8080/transactions');
-  print(response);
+  Client client =  HttpClientWithInterceptor.build(interceptors: [LoggingInterceptor()]);
+  final Response response = await client.get('http://10.0.2.2:8080/transactions');
+}
+
+class LoggingInterceptor implements InterceptorContract {
+  @override
+  Future<RequestData> interceptRequest({RequestData data}) async {
+    print('>>> sending request');
+    print('url: ${data.url}');
+    print('headers: ${data.headers}');
+    print('body: ${data.body}');
+    return data;
+  }
+
+  @override
+  Future<ResponseData> interceptResponse({ResponseData data}) async {
+    print('>>> receiving response');
+    print('status code: ${data.statusCode}');
+    print('headers: ${data.headers}');
+    print('body: ${data.body}');
+    return data;
+  }
 }
